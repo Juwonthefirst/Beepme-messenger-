1fa3db274a5a2d0b70ef0412bbb74ea201e8a7b7
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-02-16 19:19:50 +0100

    added sqlite3 database

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-import shelve, os
+import  os, sqlite3
 from flask import Flask, request, render_template
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+
 app=Flask(__name__)
 ph=PasswordHasher()
-db_location="/storage/emulated/0/Python/Beepme_messenger/database/user_database/user_database"
 
 @app.route("/")
 @app.route("/login/")
@@ -23,42 +23,45 @@ def create_user():
 				"username_unavail":"Username already in use", 					"email_not_free":"E-mail already in use",								"invalid_email":"Invalid E-mail", 												"pass_not_okay":"Password needs to be 8 characters long and only have alphabet and numbers"
 					}
 	try:
+		db=sqlite3.connect("database.db")
+		cursor=db.cursor()
 		username=(request.form.get("username")).strip(). capitalize()
 		email=(request.form.get("email")).strip().lower()
-		password=(request.form.get("password")).strip()		
-		user_db=shelve.open(db_location)
-		
-		username_avail=(not username in user_db.keys(),"username_unavail")
-		email_free=(email not in user_db["email_list"],"email_not_free")
+		password=(request.form.get("password")).strip()
+		name=cursor.execute("SELECT username FROM users WHERE username= ?",(username,)).fetchone()
+		address=cursor.execute("SELECT email FROM users WHERE email= ?",(email,)).fetchone()		
+		username_avail=(name== None,"username_unavail")
+		email_free=(address == None,"email_not_free")
 		valid_email=("@gmail.com" in email,"invalid_email")
 		pass_okay=((len(password) >=  8) and (password.isalnum()),"pass_not_okay")
 		
 		if username_avail[0] and email_free[0] and valid_email[0] and pass_okay[0]:
 			hashed_password = ph.hash(password)
-			user_db.update({username:{"email": email,"pass":hashed_password, "friends":[],"settings":{}}})
-			list=user_db["email_list"]
-			list.append(email)
-			user_db["email_list"]=list
+			cursor.execute("INSERT INTO users (username,email,pass) VALUES(?,?,?)",(username,email,hashed_password))
+			db.commit()
 			return render_template ("home.html", user=username)		
 					
 		else:
 			error = [error_dict.get(no_error[1]) for no_error in [username_avail,email_free,valid_email,pass_okay] if not no_error[0]]
 			return render_template("signup_error.html", errors=error, user=username, email=email)
-	except:
+	except ValueError:
 		error=["Unknown error, Please try again later"]
 		return render_template("signup_error.html", errors=error, user=username, email=email)
 	finally:
-		user_db.close()
+		db.close()
+
 
 @app.route("/home/", methods=["POST"])
 def login_user():
 	try:
 		password=request.form.get("password").strip()
 		username=request.form.get("username").strip().capitalize()
-		user_db=shelve.open(db_location)
-		if username in user_db.keys():
+		db=sqlite3.connect("database.db")
+		cursor=db.cursor()
+		stored_password=cursor.execute("SELECT pass FROM users WHERE username= ?",(username,)).fetchone()
+		if stored_password != None:
 			try:
-				if ph.verify(user_db[username]["pass"], password):
+				if ph.verify(stored_password[0], password):
 					return render_template("home.html", user=username)
 			except VerifyMismatchError:
 				error="Incorrect Password"
@@ -66,8 +69,9 @@ def login_user():
 		else:
 			error="Username doesn't exist"
 			return render_template("login_error.html",error=error, user=username)
-	except:
+	except ValueError:
 		error="An Error occurred, Please try again later"
 		return render_template("login_error.html",error=error, user=username)
-		
+
+				
 app.run(debug= True,host="0.0.0.0", port=5000)
